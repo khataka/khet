d489ba31871ff06acfc16e30c4ab45c07523d232
khataka <57946889+khataka@users.noreply.github.com>
2019-11-21 17:46:32 +0700

    Add files via upload

---
+++ Week2/2/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	name1 := "Khet"
+	name2 := "Com"
+	name3 := "Fang"
+	name4 := "Bam"
+	name5 := "Boat"
+	name6 := "Ice"
+	name7 := "Tan"
+	name8 := "Nanies"
+	fmt.Println(name1, name2, name3, name4, name5, name6, name7, name8)
+
+}
