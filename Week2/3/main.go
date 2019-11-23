f37d1d2b47aa63de314919f39d84de0dbbf707d6
khataka <57946889+khataka@users.noreply.github.com>
2019-11-23 18:40:23 +0700

    week 2-3

---
+++ Week2/3/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	name1 := "Jotaro"
+	name2 := "Josuke"
+	name3 := "Giorno"
+	name4 := "Jonathan"
+	name5 := "Josept"
+
+	fmt.Println(name1, name2, name3, name4, name5)
+
+}
