7fcd5df797affe11797c2bd3337ba025dbe32c5d
khataka <57946889+khataka@users.noreply.github.com>
2019-12-08 17:52:58 +0700

    Add files via upload

---
+++ week5/1/array1.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	student_1 := "Ana"
+	student_2 := "Anabell"
+	student_3 := "Annie"
+	student_4 := "Anne"
+	student_5 := "Artie"
+	student_6 := "Armellie"
+	student_7 := "Anderson"
+	student_8 := "Anakin"
+	student_9 := "Akari"
+	student_10 := "Ahri"
+
+	fmt.Println(student_1, student_2, student_3, student_4, student_5, student_6, student_7, student_8, student_9, student_10)
+
+}
