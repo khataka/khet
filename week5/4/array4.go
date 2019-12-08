5208259b4093a9b276080afd14ff8c6dbeb0c049
khataka <57946889+khataka@users.noreply.github.com>
2019-12-08 18:40:07 +0700

    array4

---
+++ week5/4/array4.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	numbers := [10]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	primes := [6]int{2, 4, 5, 6, 7, 9}
+	fmt.Println(numbers)
+	fmt.Println(primes)
+
+}
