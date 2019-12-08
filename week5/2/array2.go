c83f842519662a3d6491532fc0a1ad1cba7005c0
khataka <57946889+khataka@users.noreply.github.com>
2019-12-08 18:00:28 +0700

    array2

---
+++ week5/2/array2.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	student := []string{"Ana", "Anabell", "Annie", "Anne", "Artie", "Armellie", "Anderson", "Anakin", "Akari", "Ahri"}
+	fmt.Println(student)
+}
