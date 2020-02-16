3cefce919198234085c792cd342ed9fbf157175f
khataka <57946889+khataka@users.noreply.github.com>
2020-02-16 23:35:45 +0700

    SliceWeek4

---
+++ week5/slice/1/slice1.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	var x []int
+	length := len(x)
+	fmt.Println("Length = ", length)
+
+}
---
+++ week5/slice/2/slice2.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	names := []string{}
+	names[0] = "Ahri"
+	fmt.Println(names)
+
+}
